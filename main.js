1e93994cf538867a3630f9e23e3a73c1061bc8f3
meArchDuke <alexmarquesalves@icloud.com>
2022-07-04 13:40:55 -0300

    Feat: Added First HTML structure

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-import './style.css'
+// import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// document.querySelector("#app").innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+
+//   <img src="https://avatars.githubusercontent.com/u/85272542?v=4" alt="¡Duque avatar image" />
+// `;
+
+document.querySelector("#app").innerHTML = `
+
+  <img src="https://avatars.githubusercontent.com/u/108687308?v=4" alt="¡Duque avatar image" />
+
+  <h1>¡Duque</h1>
+  <p>@_alexmalves</p>
+
+  <ul>
+    <li>
+      <a href="https://youtube.com" target="_blank">Youtube</a>
+    </li>
+    <li>
+      <a href="https://instagram.com" target="_blank">Instagram</a>
+    </li>
+    <li>
+      <a href="https://twitter.com" target="_blank">Twitter</a>
+    </li>
+    <li>
+      <a href="https://github.com" target="_blank">Github</a>
+    </li>
+  </ul>
+`;
