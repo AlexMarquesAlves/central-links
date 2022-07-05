e9dfb727e4a0f2642471c967ee2eac0e24e0ee34
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-07-05 13:28:18 -0300

    Feat: reset css

--- main.js
+++ main.js
@@ -8,6 +8,33 @@ import "./style.scss";
 // `;
 
 document.querySelector("#app").innerHTML = `
+  <div class="container">
+      <img
+        class='avatar'
+        src="https://avatars.githubusercontent.com/u/108687308?v=4"
+        alt="¡Duque avatar image"
+      />
 
+      <h1>¡Duque</h1>
+      <p class='username'>@meArchi_Duque</p>
 
+      <ul>
+        <li>
+          <a href="https://youtube.com" target="_blank">🎬 Youtube</a>
+        </li>
+        <li>
+          <a href="https://instagram.com" target="_blank">📷 Instagram</a>
+        </li>
+        <li>
+          <a href="https://facebook.com" target="_blank">💻 Facebook</a>
+        </li>
+        <li>
+          <a href="https://github.com" target="_blank">🐱 Github</a>
+        </li>
+      </ul>
+
+      <footer>
+        <a href="#" target="_blank">Feito com ❤️ por ¡Duque</a>
+      </footer>
+  </div>
 `;
